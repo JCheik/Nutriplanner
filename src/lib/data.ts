6c42715c0d643d9b12d9e2aba2906a842365e163
Jonatan Chacón <JoniCheik@gmail.com>
2025-11-17 13:56:03 +0000

    quiero que tenga el mismo contenido que la tenia en la pantalla de auten

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -33,8 +33,8 @@ export const INITIAL_RECIPES: Recipe[] = [
 ];
 
 export const INITIAL_WEEK_PLAN: WeekPlan = [
-  { day: 'Lunes', meals: { breakfast: { id: 'm1', recipes: [] }, lunch: { id: 'm2', recipes: [] }, snack: { id: 'm22', recipes: [] }, dinner: { id: 'm3', recipes: [] } } },
-  { day: 'Martes', meals: { breakfast: { id: 'm4', recipes: [] }, lunch: { id: 'm5', recipes: [] }, snack: { id: 'm23', recipes: [] }, dinner: { id: 'm6', recipes: [] } } },
+  { day: 'Lunes', meals: { breakfast: { id: 'm1', recipes: [] }, lunch: { id: 'm2', recipes: [INITIAL_RECIPES[0]] }, snack: { id: 'm22', recipes: [] }, dinner: { id: 'm3', recipes: [] } } },
+  { day: 'Martes', meals: { breakfast: { id: 'm4', recipes: [] }, lunch: { id: 'm5', recipes: [] }, snack: { id: 'm23', recipes: [] }, dinner: { id: 'm6', recipes: [INITIAL_RECIPES[1]] } } },
   { day: 'Miércoles', meals: { breakfast: { id: 'm7', recipes: [] }, lunch: { id: 'm8', recipes: [] }, snack: { id: 'm24', recipes: [] }, dinner: { id: 'm9', recipes: [] } } },
   { day: 'Jueves', meals: { breakfast: { id: 'm10', recipes: [] }, lunch: { id: 'm11', recipes: [] }, snack: { id: 'm25', recipes: [] }, dinner: { id: 'm12', recipes: [] } } },
   { day: 'Viernes', meals: { breakfast: { id: 'm13', recipes: [] }, lunch: { id: 'm14', recipes: [] }, snack: { id: 'm26', recipes: [] }, dinner: { id: 'm15', recipes: [] } } },
